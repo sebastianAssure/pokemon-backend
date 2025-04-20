769e62bb69f8d5788082b65a4ce5087720b73473
SebastianPadillaZabala <padillasebastian140@gmail.com>
2025-04-20 18:31:15 -0400

    add: CORS configuration

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { CORS_OPTIONS } from './common/constants';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors(CORS_OPTIONS);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
