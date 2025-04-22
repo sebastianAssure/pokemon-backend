b0ae53c4b8103347c4b69be915d8a284cee55ebe
Sebastian Padilla <sebastianpadilla@ascbomacmini80.local>
2025-04-22 11:08:43 -0400

    feat: add swagger docs

--- src/pokemons/dto/create-type.dto.ts
+++ src/pokemons/dto/create-type.dto.ts
@@ -1,7 +1,9 @@
+import { ApiProperty } from "@nestjs/swagger";
 import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateTypeDto {
     
+      @ApiProperty({ example: 'Roca', description: 'Tipo del Pokémon' })
       @IsNotEmpty()
       @IsString()
       @MinLength(3)
--- src/pokemons/dto/update-level-pokemon.dto.ts
+++ src/pokemons/dto/update-level-pokemon.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, Min, Max } from 'class-validator';
 
 export class UpdateLevelPokemonDto {
+    @ApiProperty({ example: 12, minimum: 1, maximum: 100, description: 'Nivel del Pokémon (1-100)' })
     @IsInt()
     @Min(1)
     @Max(100)
